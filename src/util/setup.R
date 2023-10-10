69c0ac8533df9c713d6cd681f8540296b6f2ff95
Julian Frattini <julian.frattini@alice-dsl.net>
2023-10-10 13:57:18 +0200

    Setup analysis of wrong associations

---
+++ src/util/setup.R
@@ -0,0 +1,21 @@
+# data wrangling and manipulation
+library(tidyverse)
+
+# frequentist statistics
+library(rcompanion)
+library(psych)
+
+# defining and drawing DAGs
+library(dagitty)
+library(ggdag)
+
+# model definition, evaluation, and visualization
+library(brms)
+library(tidybayes)
+library(posterior)
+library(bayesplot)
+library(marginaleffects)
+library(broom.mixed)
+
+# figure alignment
+library(patchwork)
